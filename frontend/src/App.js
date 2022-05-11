2d079e7b2844a34e268083e2a9e82da930fa915c
Andrew McNeill <andrew@andrewmc.ca>
2022-05-11 17:55:57 -0300

    adds header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import { useState } from 'react';
-import ResultsList from './components/Results/ResultsList';
+import ResultsList from './components/Results/index';
 import Form from './components/Form';
 import Upload from './components/Upload';
+import Header from './components/Header'
 
 function App() {
 
   const [list, setList] = useState([]);
+  const [key, setKey] = useState('home');
 
   return (
     <div className="App">
-      <h1>Fun with AI</h1>
-      <Upload />
-      <br/>
-      <Form
-        setList={setList}
-      />
+      <Header />
+      <div>
+        <Upload />
+        <Form
+          setList={setList}
+        />
+      </div>
       <ResultsList
         list={list}
         setList={setList}
