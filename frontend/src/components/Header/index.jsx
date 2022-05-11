f2ab7ef6893a4dfb7cf68924f1a8ca05decaca53
Andrew McNeill <andrew@andrewmc.ca>
2022-05-11 17:57:20 -0300

    creates Header parent component

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import Headline from "./Headline";
+import About from "./About";
+
+export default function Header(props) {
+
+  return (
+    
+    <header>
+      <Navbar />
+      <Headline />
+      <About />
+    </header>
+    
+  )
+
+}
