4226bead1933da2ff134d609eaa168411144e4ae
Andrew McNeill <andrew@andrewmc.ca>
2022-05-11 17:57:07 -0300

    creates Headline component

---
+++ frontend/src/components/Header/Headline.jsx
@@ -0,0 +1,18 @@
+export default function Headline(props) {
+
+  return (
+
+    <h2>
+      <a
+        href="https://github.com/andrewlpmcneill"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Andrew McNeill
+      </a>
+      's submission for the Shopify Front End Developer Intern Challenge, May 2022.
+    </h2>
+
+  )
+
+}
