92589bb097a49235ea0e011c1e1d83906fe6579b
Andrew McNeill <andrew@andrewmc.ca>
2022-05-12 17:32:40 -0300

    adds props, a useEffect for color, and takes color from color prompt

--- frontend/src/components/Prompts/Form.jsx
+++ frontend/src/components/Prompts/Form.jsx
@@ -6,14 +6,27 @@ const axios = require('axios');
 
 export default function Form(props) {
 
-  const { setList, temperature, setTemperature, engine } = props;
+  const { setList, temperature, setTemperature, engine, onMouseOver, onMouseLeave, color, setColor } = props;
   const [prompt, setPrompt] = useState('');
   const [selected, setSelected] = useState();
+  const elements = ["Q&A", "Ads", "Product Name", "Micro-Horror", "Essay Outline"]
 
   useEffect(() => {
     setPrompt(generateTemplatePrompt(selected));
     setTemperature(generateSuggestionTemperature(selected, temperature))
   }, [selected])
+
+  useEffect(() => {
+    if (color) {
+      document.documentElement.style.setProperty('--primary', color);
+      document.getElementById("about-link").style.setProperty('color', color);
+      elements.forEach(button => {
+        document.getElementById(button).style.setProperty('background-color', color)
+        document.getElementById(button).style.setProperty('border-color', color);
+      })
+      document.getElementById("headline-link").style.setProperty('color', color);
+    }
+  }, [color])
 
   const onChange = event => {
     setPrompt(event.target.value);
@@ -25,6 +38,10 @@ export default function Form(props) {
     axios.post("/prompts", { prompt: lockedPrompt, temperature: temperature, engine: engine })
       .then(response => {
         console.log(response.data);
+        const answer = response.data;
+        if (answer[2] === "#") {
+          setColor(answer.slice(2));
+        }
         setList(prev => [...prev, { prompt: lockedPrompt, response: response.data }]);
       })
   }
@@ -46,9 +63,18 @@ export default function Form(props) {
           selected={selected}
           setSelected={setSelected}
           setTemperature={setTemperature}
+          color={color}
+          onMouseOver={onMouseOver}
+          onMouseLeave={onMouseLeave}
         />
       </div>
-      <button onClick={savePrompt}>Submit</button>
+      <button
+        onClick={savePrompt}
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave}
+      >
+        Submit
+      </button>
     </section>
     
   )
