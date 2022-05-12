5e5ce26a279479df90560882dfb8ad80b2f818b9
Andrew McNeill <andrew@andrewmc.ca>
2022-05-12 17:25:41 -0300

    adds engine selection and temperature adjustment

--- backend/routes/prompts.js
+++ backend/routes/prompts.js
@@ -5,7 +5,6 @@ const router = express.Router();
 require('dotenv').config();
 
 // openAI API constants
-const url = "https://api.openai.com/v1/engines/text-curie-001/completions";
 const config = {
   headers: {
     "Content-Type": "application/json",
@@ -13,12 +12,31 @@ const config = {
   }
 };
 
+const selectEngine = selected => {
+
+  switch (selected) {
+  default:
+    break;
+  case "davinci":
+    return "https://api.openai.com/v1/engines/text-davinci-002/completions";
+  case "curie":
+    return "https://api.openai.com/v1/engines/text-curie-001/completions";
+  case "babbage":
+    return "https://api.openai.com/v1/engines/text-babbage-001/completions";
+  case "ada":
+    return "https://api.openai.com/v1/engines/text-ada-001/completions";
+  }
+
+};
+
 // routes
 router.post('/', async(req, res) => {
 
+  const url = selectEngine(req.body.engine);
+
   const data = {
     prompt: req.body.prompt,
-    temperature: 0.5,
+    temperature: req.body.temperature,
     max_tokens: 64,
     top_p: 1.0,
     frequency_penalty: 0.0,
@@ -27,7 +45,7 @@ router.post('/', async(req, res) => {
 
   axios.post(url, data, config)
     .then(response => {
-      console.log(response.data.choices[0].text);
+      console.log(response.data);
       res.end(response.data.choices[0].text);
     });
   
