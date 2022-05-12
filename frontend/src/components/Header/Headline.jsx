82a1ac99c0a91cfe5ebb3e0901029a0ce2b19177
Andrew McNeill <andrew@andrewmc.ca>
2022-05-12 17:30:39 -0300

    adds necessary props and adds custom mouseover functions

--- frontend/src/components/Header/Headline.jsx
+++ frontend/src/components/Header/Headline.jsx
@@ -1,12 +1,25 @@
 export default function Headline(props) {
 
+  const { color, lightenDarkenColor } = props;
+
+  const onMouseOverLink = event => {
+    color ? event.target.style.color = lightenDarkenColor(color, 20) : event.target.style.color = lightenDarkenColor("#1095c1", 20);
+  }
+
+  const onMouseLeaveLink = event => {
+    color ? event.target.style.color = color : event.target.style.color = "#1095c1";
+  }
+
   return (
 
     <h2>
       <a
+        id="headline-link"
         href="https://github.com/andrewlpmcneill"
         target="_blank"
         rel="noreferrer"
+        onMouseOver={onMouseOverLink}
+        onMouseLeave={onMouseLeaveLink}
       >
         Andrew McNeill
       </a>
