b110d1e8c623ebf796c088275f9d882027d38023
Andrew McNeill <andrew@andrewmc.ca>
2022-05-13 17:13:12 -0300

    adds id for list key and color theme change logic

--- frontend/src/components/Prompts/Form.jsx
+++ frontend/src/components/Prompts/Form.jsx
@@ -9,6 +9,7 @@ export default function Form(props) {
   const { setList, temperature, setTemperature, engine, onMouseOver, onMouseLeave, color, setColor, loading, setLoading } = props;
   const [prompt, setPrompt] = useState('');
   const [selected, setSelected] = useState();
+  const [id, setId] = useState([1]);
   const elements = ["Q&A", "Ads", "Product Name", "Micro-Horror", "Essay Outline"]
 
   useEffect(() => {
@@ -30,6 +31,19 @@ export default function Form(props) {
 
   const onChange = event => {
     setPrompt(event.target.value);
+    if (selected && color) {
+      document.getElementById(selected).style.backgroundColor = color;
+      document.getElementById(selected).style.borderColor = color;
+      document.getElementById(selected).style.color = "white";
+      setSelected();
+      return;
+    }
+    if (selected) {
+      document.getElementById(selected).style.backgroundColor = "#1095c1";
+      document.getElementById(selected).style.borderColor = "#1095c1";
+      document.getElementById(selected).style.color = "white";
+      setSelected();
+    }
   }
   
   const savePrompt = event => {
@@ -40,22 +54,24 @@ export default function Form(props) {
       .then(response => {
         console.log(response.data);
         const answer = response.data;
-        if (answer[2] === "#") {
+        if (answer[2] === "#" && answer.slice(2) !== "#FFF") {
           setColor(answer.slice(2));
         }
-        setList(prev => [...prev, { prompt: lockedPrompt, response: response.data }]);
-        setLoading("false")
+        setList(prev => [...prev, { prompt: lockedPrompt, response: response.data, time: Date.now(), id: id, engine: engine }]);
+        setLoading("false");
+        setId(prev => [...prev, prev[prev.length - 1] + 1]);
+        console.log(id);
       })
   }
 
   return (
     
     <section>
-      <div className="prompt-body-headings">
+      <div className="prompt-headings">
         <h6>
           Enter prompt
         </h6>
-        <h6>
+        <h6 className="heading-suggestions">
           Suggestions
         </h6>
       </div>
