0baf2c3914ec5b73e161f7f60e1dd88c6e7902ac
Andrew McNeill <andrew@andrewmc.ca>
2022-05-13 17:14:07 -0300

    adds and reorganizes elements

--- frontend/src/components/Prompts/Header.jsx
+++ frontend/src/components/Prompts/Header.jsx
@@ -9,12 +9,10 @@ export default function Header(props) {
     <header
       className="article-header"
     >
-      <h6>
-        Modes & Settings
-      </h6>
-      <div
-        className="article-header-nav"
-      >
+      <div className="article-header-left">
+        <h6>
+          Modes
+        </h6>
         <div
           className="article-header-modes"
         >
@@ -33,92 +31,111 @@ export default function Header(props) {
             Image OCR Mode
           </button>
         </div>
-        <div
-          className="article-header-controls"
-        >
+      </div>
+      <div
+        className="article-header-right"
+      >
+        <div className='article-header-controls-engine'>
+          <h6>
+            Engine
+          </h6>
           <div
-            className="article-header-controls-temperature"
-          >
-            <h6>
-              Temperature&emsp;{temperature}
-            </h6>
-            <Slider
-              value={temperature}
-              min={0}
-              max={1}
-              step={0.1}
-              onChange={value => setTemperature(value)}
-              style={{ width: 300}}
-            />
-          </div>
-          <div
-            className="article-header-controls-engine"
+            className="article-header-controls-engine-row"
             onChange={event => setEngine(event.target.value)}
           >
-            <h6>
-              Engine
-            </h6>
-            <input
-              type="radio"
-              id="davinci"
-              name="engine"
-              value="davinci"
-              checked={engine === "davinci" ? true : false}
-              readOnly
-            />
-            <label
-              className="engine-heading"
-              data-tooltip="Most capable, but slowest"
-              htmlFor="davinci"
-            >
-              Davinci
-            </label>
-            <input
-              type="radio"
-              id="curie"
-              name="engine"
-              value="curie"
-              checked={engine === "curie" ? true : false}
-              readOnly
-            />
-            <label
-              className="engine-heading"
-              htmlFor="curie"
-              data-tooltip="Balanced between capability and speed"
-            >
-              Curie
-            </label>
-            <input
-              type="radio"
-              id="babbage"
-              name="engine"
-              value="babbage"
-              checked={engine === "babbage" ? true : false}
-              readOnly
-            />
-            <label
-              className="engine-heading"
-              htmlFor="babbage"
-              data-tooltip="Very fast at straightforward tasks"
-            >Babbage</label>
-            <input
-              type="radio"
-              id="ada"
-              name="engine"
-              value="ada"
-              checked={engine === "ada" ? true : false}
-              readOnly
-            />
-            <label
-              className="engine-heading"
-              htmlFor="davinci"
-              data-tooltip="Fastest, but only for simple tasks"
-            >
-              Ada
-            </label>
+            <div>
+              <input
+                type="radio"
+                id="davinci"
+                name="engine"
+                value="davinci"
+                checked={engine === "davinci" ? true : false}
+                readOnly
+              />
+              <label
+                className="engine-heading"
+                data-tooltip="Most capable, but slowest"
+                htmlFor="davinci"
+              >
+                Davinci
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="curie"
+                name="engine"
+                value="curie"
+                checked={engine === "curie" ? true : false}
+                readOnly
+              />
+              <label
+                className="engine-heading"
+                htmlFor="curie"
+                data-tooltip="Balanced between capability and speed"
+              >
+                Curie
+              </label>
+            </div>
+          </div>
+          <div
+            className="article-header-controls-engine-row"
+            onChange={event => setEngine(event.target.value)}
+          >
+            <div>
+              <input
+                type="radio"
+                id="babbage"
+                name="engine"
+                value="babbage"
+                checked={engine === "babbage" ? true : false}
+                readOnly
+              />
+              <label
+                className="engine-heading"
+                htmlFor="babbage"
+                data-tooltip="Very fast at straightforward tasks"
+              >
+                Babbage
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="ada"
+                name="engine"
+                value="ada"
+                checked={engine === "ada" ? true : false}
+                readOnly
+              />
+              <label
+                className="engine-heading"
+                htmlFor="davinci"
+                data-tooltip="Fastest, but only for simple tasks"
+              >
+                Ada
+              </label>
+            </div>
           </div>
         </div>
-
+        <div
+          className="article-header-controls-temperature"
+          >
+          <h6>
+            Temperature
+          </h6>
+          <Slider
+            value={temperature}
+            min={0}
+            max={1}
+            step={0.1}
+            onChange={value => setTemperature(value)}
+            style={{ width: "85%"}}
+            />
+          <h6>
+            {temperature}
+          </h6>
+        </div>
       </div>
     </header>
 
