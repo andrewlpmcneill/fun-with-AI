a1baf0629d7cdb897ff9849572b13fee1dc9a101
Andrew McNeill <andrew@andrewmc.ca>
2022-05-14 19:29:43 -0300

    adds form validation

--- frontend/src/components/Prompts/Upload.jsx
+++ frontend/src/components/Prompts/Upload.jsx
@@ -1,14 +1,30 @@
+import Errors from './Errors';
 const axios = require('axios');
 
 export default function Upload(props) {
 
-  const { id, setId, setList, setLoading } = props;
+  const { id, setId, setList, loading, setLoading, uploadError, setUploadError } = props;
 
   const onFormSubmit = event => {
     event.preventDefault();
+    setUploadError(false);
     setLoading("true");
     let data = new FormData();
-    console.log(event.target.files[0]);
+    if (!event.target.files[0]) {
+      setLoading("false");
+      console.log('rejected undefined');
+      return;
+    }
+    const file = event.target.files[0];
+    console.log(file);
+    console.log(file.type);
+    if (file.type !== "image/jpeg" && file.type !== "image/png") {
+      console.log('rejected bad file type');
+      setUploadError(true);
+      setLoading("false");
+      return;
+    }
+
     data.append('file', event.target.files[0]);
 
     axios.post('/upload', data, {
@@ -29,11 +45,37 @@ export default function Upload(props) {
 
   return (
     
-    <div>
-      <form onSubmit={onFormSubmit}>
-        <h3>Upload Image</h3>
-        <input className="imageUpload" type="file" onChange={onFormSubmit} />
-      </form>
+    <div
+      className="OCR-mode-body"
+    >
+      <div
+        className="OCR-mode-upload"
+      >
+        <form>
+          <div
+            className="modes"
+          >
+            <h6
+              aria-busy={loading === "true" ? "true" : "false"}
+            >
+              {loading === "true" ? "Uploading..." : "Upload Image"}
+            </h6>
+          </div>
+          <input
+            className="imageUpload"
+            type="file"
+            onChange={onFormSubmit}
+          />
+        </form>
+        {uploadError && <Errors error="upload"/>}
+      </div>
+      <div className="OCR-mode-text">
+        <p>
+          Select a <strong>.jpeg</strong> or <strong>.png</strong> image to upload. The server will apply optical character recognition (OCR) by TesseractJS on the image, followed by a cleanup pass by GPT-3. 
+          <br /><br />
+          Works for images of printed text only.
+        </p>
+      </div>
     </div>
     
   )
