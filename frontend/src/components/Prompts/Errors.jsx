ab5ef4ddbc2e319a80cea8c12ed12b716e3ffc9b
Andrew McNeill <andrew@andrewmc.ca>
2022-05-14 19:27:40 -0300

    creates Errors component

---
+++ frontend/src/components/Prompts/Errors.jsx
@@ -0,0 +1,26 @@
+export default function Errors(props) {
+
+  const { error } = props;
+
+  return (
+
+    <>
+      {error === "upload" && 
+      <article
+        className="alert-upload-error"
+      >
+        Invalid file type. Please select a .jpeg or .png file.
+      </article>
+      }
+      {error === "empty" && 
+      <article
+        className="alert-prompt-error"
+      >
+        The prompt is empty. Please try again.
+      </article>
+      }
+    </>
+
+  )
+
+}
