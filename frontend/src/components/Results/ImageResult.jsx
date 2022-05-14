24fe010a774c5fe9c35fa6138fb792017ab05b4c
Andrew McNeill <andrew@andrewmc.ca>
2022-05-14 14:27:19 -0300

    creates ImageResult component

---
+++ frontend/src/components/Results/ImageResult.jsx
@@ -0,0 +1,17 @@
+export default function ImageResult(props) {
+
+  const { link } = props;
+
+  return (
+
+    <a
+    href={link}
+    target="_blank"
+    rel="noreferrer"
+    >
+      Image Upload
+    </a>
+
+  )
+
+}
