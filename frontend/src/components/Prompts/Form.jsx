e5c2db572068d95170dfa389eb563221bd24e3cf
Andrew McNeill <andrew@andrewmc.ca>
2022-05-15 11:08:39 -0300

    moves effects and state to custom hooks, renames functions, cleans up code

--- frontend/src/components/Prompts/Form.jsx
+++ frontend/src/components/Prompts/Form.jsx
@@ -1,113 +1,138 @@
 import Errors from "./Errors";
-import { useState, useEffect } from "react";
-import { generateTemplatePrompt } from "../../helpers/generateTemplatePrompt";
-import { generateSuggestionTemperature } from "../../helpers/generateSuggestionTemperature";
 import Suggestions from "./Suggestions";
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 const axios = require('axios');
 
 export default function Form(props) {
 
-  const { setList, temperature, setTemperature, engine, onMouseOver, onMouseLeave, color, setColor, loading, setLoading, id, setId, promptError, setPromptError } = props;
+  const {
+    setList,
+    temperature,
+    setTemperature,
+    engine,
+    onMouseOver,
+    onMouseLeave,
+    color,
+    setColor,
+    loading,
+    setLoading,
+    id,
+    setId,
+    promptError,
+    setPromptError,
+    prompt,
+    setPrompt,
+    selectedSuggestion,
+    setSelectedSuggestion,
+  } = props;
   
-  const [prompt, setPrompt] = useState('');
-  const [selected, setSelected] = useState();
-  
-  const elements = ["Q&A", "Ads", "Product Name", "Micro-Horror", "Essay Outline"]
+  // Set prompt state, unselect suggestion button and return to app theme colour
+  const onTextAreaChange = event => {
 
-  useEffect(() => {
-    setPrompt(generateTemplatePrompt(selected));
-    setTemperature(generateSuggestionTemperature(selected, temperature))
-  }, [selected])
-
-  useEffect(() => {
-    if (color) {
-      document.documentElement.style.setProperty('--primary', color);
-      document.getElementById("about-link").style.setProperty('color', color);
-      elements.forEach(button => {
-        document.getElementById(button).style.setProperty('background-color', color)
-        document.getElementById(button).style.setProperty('border-color', color);
-      })
-      document.getElementById("headline-link").style.setProperty('color', color);
-    }
-  }, [color])
-
-  const onChange = event => {
     setPrompt(event.target.value);
-    if (selected && color) {
-      document.getElementById(selected).style.backgroundColor = color;
-      document.getElementById(selected).style.borderColor = color;
-      document.getElementById(selected).style.color = "white";
-      setSelected();
+    const element = document.getElementById(selectedSuggestion);
+    if (selectedSuggestion && color) {
+      Object.assign(element.style,{ backgroundColor: color, borderColor: color, color: "white" });
+      setSelectedSuggestion();
       return;
     }
-    if (selected) {
-      document.getElementById(selected).style.backgroundColor = "#1095c1";
-      document.getElementById(selected).style.borderColor = "#1095c1";
-      document.getElementById(selected).style.color = "white";
-      setSelected();
+    if (selectedSuggestion) {
+      Object.assign(element.style,{ backgroundColor: "#1095c1", borderColor: "#1095c1", color: "white" });
+      setSelectedSuggestion();
     }
+
   }
   
+  // Initiate loading spinners and skeleton result, send prompt and settings to server
   const savePrompt = event => {
+
     event.preventDefault();
+    // Empty form validation
     if (!prompt) {
       setPromptError(true);
       return;
     }
-    setLoading("true");
+    setLoading(true);
     const lockedPrompt = prompt;
     axios.post("/prompts", { prompt: lockedPrompt, temperature: temperature, engine: engine })
       .then(response => {
-        console.log(response.data);
         const answer = response.data;
+        // If response is a hex code, set it to system colour
         if (answer[2] === "#" && answer.slice(2) !== "#FFF") {
           setColor(answer.slice(2));
         }
+        // Update results list to trigger new result render
         setList(prev => [...prev, { prompt: lockedPrompt, response: response.data, time: Date.now(), id: id, engine: engine }]);
-        setLoading("false");
+        setLoading(false);
         setId(prev => [...prev, prev[prev.length - 1] + 1]);
-        console.log(id);
       })
+      
   }
 
   return (
     
     <section>
-      <div className="prompt-headings">
-        <div className="section-header-left">
-          <div className="modes">
+      <div
+        className="prompt-headings"
+      >
+        <div
+          className="section-header-left"
+        >
+          <div
+            className="modes"
+          >
             <h6>
               Enter prompt
             </h6>
-            <div className="tooltip" data-tooltip="Enter text to generate responses">
-              <AiOutlineInfoCircle className="tooltip" style={{marginBottom: "4px"}} />
+            <div
+              className="tooltip"
+              data-tooltip="Enter text to generate responses"
+            >
+              <AiOutlineInfoCircle
+                className="tooltip"
+                style={{marginBottom: "4px"}}
+              />
             </div>
           </div>
-          <textarea id="prompt-input" onChange={onChange}></textarea>
+          <textarea
+            id="prompt-input"
+            onChange={onTextAreaChange}
+          />
           <button
             id="submit"
             onClick={savePrompt}
             onMouseOver={onMouseOver}
             onMouseLeave={onMouseLeave}
-            aria-busy={loading}
+            aria-busy={loading ? "true" : "false"}
           >
-            {loading === "true" ? "" : "Submit"}
+            {loading ? "" : "Submit"}
           </button>
         </div>
-        <div className="section-header-right">
-          <div className="modes">
+        <div
+          className="section-header-right"
+        >
+          <div
+            className="modes"
+          >
             <h6>
               Suggestions
             </h6>
-            <div className="tooltip" data-tooltip="Use a pre-made prompt">
-              <AiOutlineInfoCircle className="tooltip" style={{marginBottom: "4px"}} />
+            <div
+              className="tooltip"
+              data-tooltip="Use a pre-made prompt"
+            >
+              <AiOutlineInfoCircle
+                className="tooltip"
+                style={{marginBottom: "4px"}}
+              />
             </div>
           </div>
-          <div className="prompt-body-content">
+          <div
+            className="prompt-body-content"
+          >
             <Suggestions
-              selected={selected}
-              setSelected={setSelected}
+              selectedSuggestion={selectedSuggestion}
+              setSelectedSuggestion={setSelectedSuggestion}
               setTemperature={setTemperature}
               color={color}
               onMouseOver={onMouseOver}
