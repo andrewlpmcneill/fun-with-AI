4a1f6115be4dc78764981aa373d6bbddbf27e1f4
Andrew McNeill <andrew@andrewmc.ca>
2022-05-15 11:06:09 -0300

    moves functions to custom hooks, cleans up code

--- frontend/src/components/Header/Headline.jsx
+++ frontend/src/components/Header/Headline.jsx
@@ -1,14 +1,9 @@
 export default function Headline(props) {
 
-  const { color, lightenDarkenColor } = props;
-
-  const onMouseOverLink = event => {
-    color ? event.target.style.color = lightenDarkenColor(color, 20) : event.target.style.color = lightenDarkenColor("#1095c1", 20);
-  }
-
-  const onMouseLeaveLink = event => {
-    color ? event.target.style.color = color : event.target.style.color = "#1095c1";
-  }
+  const {
+    onMouseOverLink,
+    onMouseLeaveLink
+  } = props;
 
   return (
 
